783cc6b030e9b8246b4c726d4c8480d7af6f66b2
Neeraj Shimpi <76657706+ASTRO-N1@users.noreply.github.com>
2025-10-26 12:43:21 +0530

    Wrap App component with LocalizationProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/globals.css";
 import "./index.css";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </LocalizationProvider>
 );
